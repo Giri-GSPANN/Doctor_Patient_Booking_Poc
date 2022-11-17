4eabf497ee6c0ed55adf58bd8d8dc282a732199b
Giri-GSPANN <118428269+Giri-GSPANN@users.noreply.github.com>
2022-11-17 13:12:01 +0530

    Add files via upload
    * First commit.

---
+++ Doctor_Booking_Poc/Doctor_Booking_Poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doctor_Booking_Poc
+//
+//  Created by Giri Thangellapally on 17/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Doctor_Booking_Poc/Doctor_Booking_Poc/Doctor_Booking_PocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_Booking_PocApp.swift
+//  Doctor_Booking_Poc
+//
+//  Created by Giri Thangellapally on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_Booking_PocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
